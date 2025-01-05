45b04556c75e0e2c24720d80b29da77fcd10eaab
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2025-01-05 15:55:32 -0300

    Se edito el .estlint

---
+++ .estlintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'next/core-web-vitals',
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error', // Or 'warn' if you want less strict
+      'no-unused-vars': 'error', // Or 'warn' if you want less strict
+    },
+  }
+  
+  
